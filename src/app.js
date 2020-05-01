ead6a300af98f21f47a348afb4446131638c4ffa
decio.montanhani <decio.montanhani@iteris.com.br>
2020-05-01 20:25:09 -0300

    criando rotas

--- src/app.js
+++ src/app.js
@@ -1,33 +1,91 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function validateIdFormat(request, response, next) {
+
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    response.status(400).json({ error: "Invalid Id" });
+    return
+  }
+
+  return next();
+}
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+
+  const repo = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repo);
+
+  response.json(repo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateIdFormat, (request, response) => {
+
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if (index < 0) {
+    response.status(400).json({ error: "Invalid Id" })
+  }
+
+  let repo = repositories[index]
+
+  let newInfo = { ...repo, title, url, techs };
+
+  repositories[index] = newInfo;
+
+  response.json(newInfo);
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateIdFormat, (request, response) => {
+
+  const { id } = request.params;
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if (index < 0) {
+    response.status(400).json({ error: "Invalid Id" })
+  }
+
+  repositories.splice(index, 1);
+
+  response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateIdFormat, (request, response) => {
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if (index < 0) {
+    response.status(400).json({ error: "Invalid Id" })
+  }
+
+  repositories[index].likes += 1
+
+  response.json({ likes: repositories[index].likes });
+
 });
 
 module.exports = app;
